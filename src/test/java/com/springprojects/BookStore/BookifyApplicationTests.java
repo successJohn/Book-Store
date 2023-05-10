c26a6cd6f19e3e893f13bc8e13bd264eaa90115a
Success John <successojohn@gmail.com>
2023-05-10 21:18:46 +0100

    Project Setup

---
+++ src/test/java/com/springprojects/BookStore/BookifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springprojects.BookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
