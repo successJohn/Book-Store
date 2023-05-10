3374295a600aa79c6d13f435f1593e8444538fd2
Success John <successojohn@gmail.com>
2023-05-10 23:21:28 +0100

    Revert "User entity and repository config"
    This reverts commit d9d6b45d4a4f7862d2bf7e3f2c2ce9ed6d5762d9.

--- src/main/java/com/springprojects/BookStore/BookifyApplication.java
+++ src/main/java/com/springprojects/BookStore/BookifyApplication.java
@@ -2,21 +2,12 @@ package com.springprojects.BookStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-@SpringBootApplication(exclude= {SecurityAutoConfiguration.class})
+
+@SpringBootApplication
 public class BookifyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookifyApplication.class, args);
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder(){
-		return new BCryptPasswordEncoder(11);
 	}
 
 }
--- src/main/java/com/springprojects/BookStore/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.springprojects.BookStore.service;
-
-import com.springprojects.BookStore.entity.User;
-import com.springprojects.BookStore.model.UserModel;
-
-public interface UserService {
-     User registerUser(UserModel user);
-}
